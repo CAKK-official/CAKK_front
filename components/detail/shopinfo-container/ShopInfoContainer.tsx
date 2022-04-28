6fbda9656cf57526fcf0be2fe1bf9872a364ab20
Hyevvy <hyeg0@naver.com>
2022-04-29 01:56:56 +0900

    ✨ feat: category 컴포넌트 구현

--- components/detail/shopinfo-container/ShopInfoContainer.tsx
+++ components/detail/shopinfo-container/ShopInfoContainer.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { css } from '@emotion/react'
+import { css, jsx } from '@emotion/react'
+import Category from '../category/Category'
 
+const FONT_COLOR = 'E72D3F'
 interface ShopInfoInterface {
   title: string
   desc: string
+  categories: Array<string>
 }
-const ShopInfoContainer: React.FC<ShopInfoInterface> = ({ title, desc }) => {
+const ShopInfoContainer: React.FC<ShopInfoInterface> = ({
+  title,
+  desc,
+  categories,
+}) => {
   return (
     <div>
       <h2>{title}</h2>
@@ -16,6 +23,11 @@ const ShopInfoContainer: React.FC<ShopInfoInterface> = ({ title, desc }) => {
       >
         {desc}
       </h3>
+      <div style={{ display: 'flex' }}>
+        {categories.map((category: string) => (
+          <Category key={category} category={category} />
+        ))}
+      </div>
     </div>
   )
 }
--- pages/detail.tsx
+++ pages/detail.tsx
@@ -5,6 +5,31 @@ import Layout from '../components/layout'
 import React from 'react'
 import { ShopInfoContainer } from '../components/detail/shopinfo-container'
 
+const DUMMYDATA = {
+  id: 1,
+  name: '을지로 앵초 케이크',
+  address: '서울특별시 중구 창경궁로5길 18',
+  tel: '02-402-4590',
+  notice:
+    '직접 구운 촉촉한 시트와 크림치즈크림을 사용합니다. 케이크예약은 카카오톡채널 “을지로앵초” 디엠X',
+  url: 'Dummyurl',
+  menu: 'Dummymenu',
+  beforebuy:
+    '### Vestibulum ante ipsum primis #### in faucibus orci luctus et ultrices  posuere cubilia Curae; Duis faucibus accumsan odio. ',
+  afterbuy:
+    '### Vestibulum ante ipsum primis #### in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curab',
+  whenbuy:
+    '### Vestibulum ante ipsum primis #### in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curab',
+  opened: '화요일 - 일요일 13:00 - 19:00',
+  closed: '매주 월요일 정기 휴무, 화요일 격주 휴무',
+  latlng: [12.323123421, 23.2312412412],
+  pictArray: [
+    'http://dummyimage.com/176x100.png/ff4444/ffffff',
+    'http://dummyimage.com/246x100.png/5fa2dd/ffffff',
+  ],
+  storeCategory: ['photo', 'lunch', 'tiara', 'letter', 'solid'],
+}
+
 //TODO : 상의후 BannerContainer에 slidesPerView 인자로 넘겨주기
 const Detail: NextPage = () => {
   return (
@@ -16,8 +41,9 @@ const Detail: NextPage = () => {
       </Head>
       <BannerContainer />
       <ShopInfoContainer
-        title="을지로 앵초"
-        desc="직접 구운 촉촉한 시트와 크림치즈크림을 사용합니다. 케이크예약은 카카오톡채널 “을지로앵초” 디엠X"
+        title={DUMMYDATA.name}
+        desc={DUMMYDATA.notice}
+        categories={DUMMYDATA.storeCategory}
       />
     </Layout>
   )
