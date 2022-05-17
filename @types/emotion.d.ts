48e67f5d80e44bb14b12fedf17cf2cb904d6b831
Dongha Kang <dkang0602@gmail.com>
2022-05-17 18:07:05 +0900

    :lipstick: style: set up colors (style guide)

--- @types/emotion.d.ts
+++ @types/emotion.d.ts
@@ -3,10 +3,25 @@ import '@emotion/react'
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
-      primary: string
-      borderColor: string
-      backgroundColor: string
-      fontColor: string
+      // primary: string
+      // borderColor: string
+      // backgroundColor: string
+      // fontColor: string
+
+      // Style guide
+      red: string
+      pink: {
+        A80: string
+        A50: string
+        A20: string
+      }
+      black: string
+      gray: {
+        A80: string
+        A50: string
+        A20: string
+      }
+      white: string
     }
     screen: {
       main: string
--- assets/theme/theme.ts
+++ assets/theme/theme.ts
@@ -1,11 +1,28 @@
 import { Theme } from '@emotion/react'
 
 const colors = {
-  primary: '#EE8288',
-  backgroundColor: '#F9CCC9',
+  // primary: '#EE8288',
+  // backgroundColor: '#F9CCC9',
 
-  borderColor: '#FBF2F4',
-  fontColor: '#707070',
+  // borderColor: '#FBF2F4',
+  // fontColor: '#707070',
+
+  // Style guide
+  red: '#E72E40',
+  pink: {
+    A80: '#EE8288',
+    A50: '#F9CCC9',
+    A20: '#FEF6F4',
+  },
+
+  black: '#383838',
+  gray: {
+    A80: '#707070',
+    A50: '#EAEAEA',
+    A20: '#F6F6F6',
+  },
+
+  white: '#FFFFFF',
 }
 
 const screen = {
