da181af8835d3f3df4a78dbc6cee2f303fc7c5f5
Dongha Kang <dkang0602@gmail.com>
2022-05-20 16:25:34 +0900

    :truck: chore: Added Link to detail page from main page
    - 현재로서는 detail/1 이런식으로는 작동 되지는 않는다. /detail/ 로 변경하면 코드는 작동된다

--- components/item-container/ItemGridContainer.tsx
+++ components/item-container/ItemGridContainer.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { PopularResponse } from '../../src/api/api'
 import Icon from '../icon'
 import * as Styled from './style'
+import Link from 'next/link'
+
 interface ItemGridInterface {
   //TODO: update props, fetched data
   row: number
@@ -19,29 +21,35 @@ const ItemGridContainer: React.FC<ItemGridInterface> = ({
     <Styled.ItemGridContainer rows={row}>
       {items.map((item: PopularResponse, index: number) => (
         <div className="item-grid-item" key={index}>
-          <div className="item-container">
-            <div className="image-container">
-              <img src={item.picture} />
-              {/* <Image
+          <Link href={`/detail/${item.id}`} passHref prefetch={false}>
+            <div className="item-container">
+              <div className="image-container">
+                <img src={item.picture} />
+                {/* <Image
                 src={item.picture}
                 alt="Image not found"
                 layout="fill"
                 objectFit="cover"
               /> */}
-            </div>
-            <div className="information-container">
-              <div className="info-text-container">
-                <h4>{item.name}</h4>
-                <span>{item.address}</span>
               </div>
-              <div className="button-container">
-                <button>
-                  <Icon name="icon_fork_fill" width={24} height={24} />
-                </button>
+              <div className="information-container">
+                <div className="info-text-container">
+                  <h4>{item.name}</h4>
+                  <span>{item.address}</span>
+                </div>
+                <div className="button-container">
+                  <button
+                    onClick={() => {
+                      console.log('fork clicked')
+                    }}
+                  >
+                    <Icon name="icon_fork_fill" width={24} height={24} />
+                  </button>
+                </div>
               </div>
+              {ranking && <div className="ranking">{index + 1}</div>}
             </div>
-            {ranking && <div className="ranking">{index + 1}</div>}
-          </div>
+          </Link>
         </div>
       ))}
     </Styled.ItemGridContainer>
