5c6617bf0102745a7fdb36abff22f8c5de68529d
Dongha Kang <dkang0602@gmail.com>
2022-05-21 15:38:09 +0900

    Feature/main api (#14)
    * :lipstick: style: set up fonts (style guide)
    
    * :lipstick: style: set up colors (style guide)
    
    * :lipstick: style: apply style guide in all components
    
    * :sparkles: feat: api for main page
    
    * :sparkles: feat: proxy 구현
    
    * :bug: fix: Fix api issue
    
    * :truck: chore: Added Link to detail page from main page
    
    - 현재로서는 detail/1 이런식으로는 작동 되지는 않는다. /detail/ 로 변경하면 코드는 작동된다
    
    * :sparkles: feat: location/category api
    
    Co-authored-by: Hyevvy <hyeg0@naver.com>

--- components/main/rank-section/RankContainer.tsx
+++ components/main/rank-section/RankContainer.tsx
@@ -1,33 +1,34 @@
 import { ItemGridContainer } from '../../item-container'
+import { ItemResponseProps } from '../../../src/api/api'
 
 import * as Styled from './style'
 
-const RankContainer = () => {
-  const toyData = [
-    {
-      id: 1,
-      name: '키키네하우스',
-      address: '송파구 오금로 130',
-      img: 'https://picsum.photos/200/300',
-    },
-    {
-      id: 2,
-      name: '케키하우스',
-      address: '백제 고분로 41길 35',
-      img: 'https://picsum.photos/200/300',
-    },
-    {
-      id: 3,
-      name: '희스케이크',
-      address: '백제고분로27길 26',
-      img: 'https://picsum.photos/200/300',
-    },
-  ]
+const RankContainer: React.FC<{ data: ItemResponseProps[] }> = ({ data }) => {
+  // const toyData = [
+  //   {
+  //     id: 1,
+  //     name: '키키네하우스',
+  //     address: '송파구 오금로 130',
+  //     img: 'https://picsum.photos/200/300',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: '케키하우스',
+  //     address: '백제 고분로 41길 35',
+  //     img: 'https://picsum.photos/200/300',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: '희스케이크',
+  //     address: '백제고분로27길 26',
+  //     img: 'https://picsum.photos/200/300',
+  //   },
+  // ]
 
   return (
     <Styled.RankContainer>
       <h2>인기 케이크 가게 Top3</h2>
-      <ItemGridContainer ranking row={3} items={toyData} />
+      <ItemGridContainer ranking row={3} items={data} />
     </Styled.RankContainer>
   )
 }
