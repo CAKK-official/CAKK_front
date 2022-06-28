5be8982df8ed79decba9658d35e0db53fcb80745
Dongha Kang <dkang0602@gmail.com>
2022-06-28 14:50:38 +0900

    :bug: fix: getStaticProps with api / useEffect (#35)

--- pages/detail/[detail_id].tsx
+++ pages/detail/[detail_id].tsx
@@ -2,38 +2,64 @@ import { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import * as Styled from '../../styles/home'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ShopInfoContainer } from '../../components/detail/shopinfo-container'
 import { ItemSwiperContainer } from '../../components/item-container'
 import { DetailResponse, fetchDetail } from '../../src/api/api'
+import { useRouter } from 'next/router'
 
-const Detail: NextPage<{ data: DetailResponse[] }> = ({ data }) => {
-  console.log('DaTA!', data)
+const Detail: NextPage = () => {
+  const { query } = useRouter()
+  const [data, setData] = useState<DetailResponse[]>([])
+
+  // console.log(route);
+  useEffect(() => {
+    const fetchDetailData = async () => {
+      try {
+        const fetchedData = await fetchDetail(
+          parseInt(query.detail_id as string)
+        )
+        setData(fetchedData)
+      } catch (err) {
+        console.error('Failed to fetch data in /detail/<detail id>', err)
+      }
+    }
+    // console.log(query.detail_id)
+    if (query.detail_id !== undefined) {
+      fetchDetailData()
+    }
+  }, [query.detail_id])
 
   return (
     <Layout>
       <Head>
-        <title>케이크크 | 🎂{data[0].name}</title>
+        {/* <title>케이크크 | {data ? `🎂${data[0].name}` : 'Loading...'}</title> */}
+        <title>케이크크 | Loading...</title>
         <link rel="icon" href="/favicon.ico" />
         <script
           defer
           src="https://developers.kakao.com/sdk/js/kakao.min.js"
         ></script>
       </Head>
-      <Styled.Home>
-        <ItemSwiperContainer count={3} items={data[0].pictArray} />
-        <ShopInfoContainer
-          id={data[0].id}
-          title={data[0].name}
-          desc={data[0].notice}
-          categories={data[0].storeCategory}
-          tel={data[0].tel}
-          opened={data[0].opened}
-          closed={data[0].closed}
-          url={data[0].url}
-          latlng={data[0].latlng}
-        />
-      </Styled.Home>
+      <div></div>
+      {data.length !== 0 ? (
+        <Styled.Home>
+          <ItemSwiperContainer count={3} items={data[0].pictArray} />
+          <ShopInfoContainer
+            id={data[0].id}
+            title={data[0].name}
+            desc={data[0].notice}
+            categories={data[0].storeCategory}
+            tel={data[0].tel}
+            opened={data[0].opened}
+            closed={data[0].closed}
+            url={data[0].url}
+            latlng={data[0].latlng}
+          />
+        </Styled.Home>
+      ) : (
+        <div>Loading</div>
+      )}
     </Layout>
   )
 }
@@ -45,19 +71,19 @@ const Detail: NextPage<{ data: DetailResponse[] }> = ({ data }) => {
 //   }
 // }
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  // Fetch Detail before render
-  const data = await fetchDetail(parseInt(params?.detail_id as string))
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   // Fetch Detail before render
+//   let data = [] as DetailResponse[]
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+//   try {
+//     data = await fetchDetail(parseInt(params?.detail_id as string))
+//   } catch (err) {
+//     console.error('Failed to fetch data in /detail/<detail id>', err)
+//   }
 
-  return {
-    props: { data }, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   }
+// }
 
 export default Detail
