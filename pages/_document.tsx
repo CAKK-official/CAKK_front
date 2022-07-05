15de8fc747930b79eb313920ea7e4c0701629077
Dongha Kang <dkang0602@gmail.com>
2022-07-05 11:19:54 +0900

    :bug: fix: mixed content issue fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
         <script
           defer
           src="https://developers.kakao.com/sdk/js/kakao.min.js"
