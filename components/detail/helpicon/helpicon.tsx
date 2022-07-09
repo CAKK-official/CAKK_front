0ce0c2f6b066654168fae4680ce4205635af6874
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-07-09 15:34:40 +0900

    :sparkles: feat: helpIcon 구현 (#45)
    * :sparkles: feat: helpIcon 구현
    
    * :sparkles: feat: help icon 클릭시 구글폼으로 이동하는 기능 구현

---
+++ components/detail/helpicon/helpicon.tsx
@@ -0,0 +1,25 @@
+import * as Styled from './style'
+import theme from '../../../assets/theme/theme'
+import Icon from '../../icon'
+
+const HelpIcon: React.FC = () => {
+  return (
+    <a
+      target="_blank"
+      href="https://forms.gle/UKDrWbytYf32cEAZ9"
+      rel="noopener noreferrer"
+    >
+      <Styled.helpIconBox>
+        <div>케이크크를</div>
+        <Styled.redSpan>도와주세요!</Styled.redSpan>
+        <Icon
+          name="icon_cake_fill"
+          width={62}
+          height={62}
+          color={theme.colors.red}
+        />
+      </Styled.helpIconBox>
+    </a>
+  )
+}
+export default HelpIcon
