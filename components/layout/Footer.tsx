87248fb0a92f244a7b87ef9232477a0ee4a0f153
Hyevvy <hyeg0@naver.com>
2022-07-17 23:08:54 +0900

    feat: footer 구현

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,21 @@
 import * as Styled from './style'
+import Link from 'next/link'
+import InstagramIcon from '@mui/icons-material/Instagram'
 
 const Footer = () => {
-  return <Styled.Footer>Footer goes here</Styled.Footer>
+  return (
+    <Styled.Footer>
+      <Styled.FooterInnerBox>
+        <strong>CAKK</strong> | <Link href="/about"> Brand Story</Link>
+      </Styled.FooterInnerBox>
+      <div>© 2022 CAKK all rights reserved</div>
+      <Styled.Icon>
+        <Link href="#">
+          <InstagramIcon />
+        </Link>
+      </Styled.Icon>
+    </Styled.Footer>
+  )
 }
 
 export default Footer
